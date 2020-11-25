5c3c4eb09abc53e7c10a7b33c1c754bec7278ecd
beesan atawna <171006@ppu.edu.ps>
2020-11-25 09:22:34 +0200

    Initial Commit

---
+++ lib/Restaurants.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+class Restaurants {
+  List<Restaurants> restaurantsFromJson(String str) {
+    return List<Restaurants>.from(
+        json.decode(str).map((x) => Restaurants.fromJson(x)));
+  }
+
+  int id;
+  String name;
+  String city;
+  String lat;
+  String lng;
+  String phone;
+  String image;
+  int rating;
+
+  Restaurants({
+    this.id,
+    this.name,
+    this.city,
+    this.lat,
+    this.lng,
+    this.phone,
+    this.image,
+    this.rating,
+  });
+
+  factory Restaurants.fromJson(dynamic json) {
+    return Restaurants(
+      id: json["id"],
+      name: json["name"],
+      city: json["city"],
+      lat: json["lat"],
+      lng: json["lng"],
+      phone: json["phone"],
+      image: json["image"],
+      rating: json["rating"],
+    );
+  }
+}
---
+++ lib/menu.dart
@@ -0,0 +1,34 @@
+class Menu {
+  int id;
+  int restId;
+  String name;
+  String descr;
+  int price;
+  String image;
+  dynamic rating;
+  String men;
+
+  Menu(
+      {this.id,
+      this.restId,
+      this.name,
+      this.descr,
+      this.price,
+      this.image,
+      this.rating,
+      this.men});
+
+  factory Menu.fromJson(dynamic json) {
+    String M = json['id'];
+    return Menu(
+      id: json["id"],
+      restId: json["rest_id"],
+      name: json["name"],
+      descr: json["descr"],
+      price: json["price"],
+      image: json["image"],
+      rating: json["rating"],
+      men: 'http://appback.ppu.edu/menus/$M',
+    );
+  }
+}
