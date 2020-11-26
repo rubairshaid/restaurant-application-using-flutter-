e67612cb4e8f11dc126a2b8f294cb8ea7ae129a1
beesan atawna <171006@ppu.edu.ps>
2020-11-26 11:49:46 +0200

    mainpage update

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:talabat/Restaurants.dart';
 
@@ -11,24 +8,20 @@ class MainPage extends StatefulWidget {
   MainPage(this.restaurants);
 
   @override
-  _MainPageState createState() => _MainPageState();
+  _MainPageState createState() => _MainPageState(restaurants);
 }
 
 class _MainPageState extends State<MainPage> {
-  Future<List<Restaurants>> restaurants;
+
+  List<Restaurants> res ;
+
+  _MainPageState(this.res);
+
   String fromName;
   String fromCity;
   String fromImage;
   int fromRate;
   int fromId;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   fromId = json['id'];
-  //   restaurants = fetchRestaurant(fromId);
-  //
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,75 +35,34 @@ class _MainPageState extends State<MainPage> {
       ),
       body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              border: Border(
-                left: BorderSide(
-                  color: Colors.deepOrangeAccent[100],
-                  width: 3,
-                ),
-              ),
-            ),
-            height: 50,
-            color: Colors.deepOrangeAccent[100],
-            margin: EdgeInsets.all(10.0),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 40,
-                child: CircleAvatar(
-                  backgroundImage: ExactAssetImage(fromImage),
-                  minRadius: 40,
-                  maxRadius: 40,
-                ),
-              ),
-              title: Text(fromName),
-              subtitle: Text(fromCity),
-            ),
-          ),
-          FutureBuilder(
-            future: restaurants,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<Restaurants> res = snapshot.data;
-                if (res.isEmpty) {
-                  return SpinKitThreeBounce(
-                    color: Colors.orangeAccent[500],
-                    size: 50,
-                  );
-                }
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: res.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        color: Colors.orangeAccent,
-                        margin: EdgeInsets.all(8.0),
-                        child: ListTile(
-                          onTap: () {
-                            fromImage = res[index].image;
-                            fromName = res[index].name;
-                            fromRate = res[index].rating;
-                            res.clear();
-                            setState(() {});
-                            // restaurants = fetchRestaurant(fromId);
-                          },
-                          leading: Image.network(res[index].image),
-                          title: Text(res[index].name),
-                          subtitle: Text('${res[index].rating}'),
-                        ),
-                      );
+          Text("kekekekkekekke"),
+          Expanded(
+            child: ListView.builder(
+
+              itemCount: res.length,
+              itemBuilder: (context, index) {
+                if (res.length == null)
+                print ("hehehehhehehehehehheeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+                print (res.length);
+                return Container(
+                  color: Colors.orangeAccent,
+                  margin: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    onTap: () {
+                      fromImage = res[index].image;
+                      fromName = res[index].name;
+                      fromRate = res[index].rating;
+                      res.clear();
+                      setState(() {});
+                      // restaurants = fetchRestaurant(fromId);
                     },
+                    leading: Image.network(res[index].image),
+                    title: Text(res[index].name),
+                    subtitle: Text('${res[index].rating}'),
                   ),
                 );
-              } else {
-                return SpinKitThreeBounce(
-                  color: Colors.deepOrange[500],
-                  size: 50,
-                );
-              }
-            },
+              },
+            ),
           ),
         ],
       ),
