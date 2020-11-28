9e95513c51966f6d7ebbf20d306bb3841be6e252
beesan atawna <171006@ppu.edu.ps>
2020-11-28 22:57:07 +0200

    ruba

--- lib/appBarBody.dart
+++ lib/appBarBody.dart
@@ -15,6 +15,7 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
         title: Text("$resName menu" ),
         actions: [
           IconButton(
