5cf1b5716ce2bc134d763dfeb37ab67f17826ce8
beesan atawna <171006@ppu.edu.ps>
2020-11-28 23:18:20 +0200

    beesan

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -17,9 +17,9 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   List<Restaurants> res;
-  List<Restaurants> forNullText ;
+  List<Restaurants> forNullText;
 
-  _MainPageState(this.res){
+  _MainPageState(this.res) {
     forNullText = res;
   }
 
@@ -89,17 +89,14 @@ class _MainPageState extends State<MainPage> {
                 ),
               ),
               onSubmitted: (text) {
-                print (text);
+                print(text);
                 text = text.toLowerCase();
-                if(text=="")
-                {
-                  res=forNullText;
-                  setState(() {
-                    
-                  });
-                  return ;
+                if (text == "") {
+                  res = forNullText;
+                  setState(() {});
+                  return;
                 }
-                
+
                 setState(() {
                   res = res.where((element) {
                     var cityTitle = element.city.toLowerCase();
@@ -205,30 +202,27 @@ class _MainPageState extends State<MainPage> {
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
                                       FlatButton(
-                                        color: Colors.orangeAccent,
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(12)),
-                                        child: Container(
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.end,
-                                            children: [
-                                              Icon(
-                                                Icons.menu,
-                                                color: Colors.indigo[900],
-                                              ),
-                                            ],
+                                          child: Container(
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                Icon(
+                                                  Icons.menu,
+                                                  color: Colors.indigo[900],
+                                                ),
+                                              ],
+                                            ),
                                           ),
-                                        ),
-                                        onPressed: (){
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) =>  Header(res[index].id , res[index].name),
-                                              ));
-                                        }
-                                      ),
+                                          onPressed: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) => Header(
+                                                      res[index].id,
+                                                      res[index].name),
+                                                ));
+                                          }),
                                       FlatButton(
                                         color: Colors.orangeAccent,
                                         shape: RoundedRectangleBorder(
@@ -291,4 +285,3 @@ class _MainPageState extends State<MainPage> {
         });
   }
 }
-
